2f39f9e828a19b19daa286aeb4f7b8072c9da5e0
jondlc02 <jondlc02@gmail.com>
2022-12-29 18:53:17 -0500

    Added functionality to create tags

--- bagbot.js
+++ bagbot.js
@@ -1,18 +1,47 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const Sequelize = require('sequelize');
-const {Client, Events, GatewayIntentBits, InteractionResponse, CommandInteractionOptionResolver} = require('discord.js');
+const {Client, Events, GatewayIntentBits, Collection} = require('discord.js');
 const {token} = require('./config.json'); // Curly brackets give access to definition of same name
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
-const sequelize = new Sequelize('database', 'user', 'password', 
+/*---- This part of the code loads all the command files into one handy dandy collection ----*/
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');  // Helps to construct a path to the commands directory for ease of use
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); // fs.readdirSync reads path to directory and returns array of all file names in dir
+// filter only returns files with .js at the end
+
+for (const file of commandFiles) {    // Loops through array of files with .js at the end
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);    // Sets the files into the 'commands' collection
+    }
+    else {
+        if ('execute' in command)
+        {
+            console.log(`No data :(`);
+        }
+        else if ('data' in command)
+        {
+            console.log(`No execute :(`);
+        }
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" and/or "execute" property.`)    // Throws an error if data or execute is missing from the command
+    }
+}
+
+/*const sequelize = new Sequelize('database', 'username', 'password', 
 {
 	host: 'localhost',
 	dialect: 'sqlite',
 	logging: false,
 	storage: 'database.sqlite',
-});
+});*/
 
-const Tags = sequelize.define('tags', { // This defines the models of the database
+/*const Tags = sequelize.define('tags', { // This defines the models of the database
 	name: 
     { // Specifies that the name part of the tag is a string/text and that each name
 		type: Sequelize.TEXT, // Must be unique to be entered into the database
@@ -26,11 +55,12 @@ const Tags = sequelize.define('tags', { // This defines the models of the databa
 		defaultValue: 0,
 		allowNull: false,
 	},
-});
+});*/
 
+//const Tags = require('./dbInit.js')
 client.once(Events.ClientReady,() => 
 {
-    Tags.sync(); // Tables are only created if they get sync-ed
+    //Tags.sync(); // Tables are only created if they get sync-ed
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
@@ -38,10 +68,26 @@ client.on(Events.InteractionCreate, async interaction =>
 {
     if (!interaction.isChatInputCommand()) return;
 
-    const {commandName} = interaction;
+    const command = interaction.client.commands.get(interaction.commandName);
 
-    // Adds tag into the database
-    if (commandName === 'addTag') 
+    console.log(`${command}`);
+    if (!command)
+    {
+        console.error(`No command matching ${interaction.commandName} was found`);
+        return;
+    }
+
+    try
+    {
+        await command.execute(interaction);
+    }
+    catch (error)
+    {
+        console.error(`Error executing ${interaction.commandName}`);
+		console.error(error);
+    }
+
+    /*if (commandName === 'addtag') 
     {
         const tagName = interaction.options.getString('name');
         const tagDescription = interaction.options.getString('description');
@@ -51,7 +97,7 @@ client.on(Events.InteractionCreate, async interaction =>
             {
                 name: tagName,
                 description: tagDescription,
-                username: interaction.user.username
+                username: interaction.author.username
             });
 
             return interaction.reply(`Tag ${tagName} added.`)
@@ -67,7 +113,7 @@ client.on(Events.InteractionCreate, async interaction =>
     }
 
     // Allows for ssearching of tag with a given name, I have no idea the algorithm
-    else if (command === 'tag') 
+    if (command === 'tag') 
     {
         const tagName = interaction.options.getString('name');
 
@@ -136,7 +182,7 @@ client.on(Events.InteractionCreate, async interaction =>
             return interaction.reply(`Tag deleted.`)
         }
         return interaction.reply(`No tag found with name ${tagName}`);
-    }
+    }*/
 });
 
 client.login(token);
